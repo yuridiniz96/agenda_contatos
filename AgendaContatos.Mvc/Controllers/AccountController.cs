8f941633e2d7645fa8f095af58b4e3a9961f740a
yuridiniz96 <yuri.anselmo.diniz@gmail.com>
2022-07-28 18:40:54 -0300

    first commit

---
+++ AgendaContatos.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaContatos.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto AspNet MVC para Controle de Contatos
+Treinamento C# WebDeveloper COTI Informática
+
+## Tecnologias utilizadas:
+
+* AspNet MVC
+* Bootstrap
+* Dapper e acesso ao SqlServer
+* Autenticação por Cookies
+* Envio de email
+* Jquery (Javascript)
+* Relatórios
